58fb813e4b974f5cc00fbe92e78a69cae247c4c7
Naphat <naphat.mars@gmail.com>
2025-11-11 11:19:06 +0700

    test: update test_scratch.sh

--- src/clevr_tasks/test_scratch.sh
+++ src/clevr_tasks/test_scratch.sh
@@ -13,11 +13,12 @@ echo $MAX_Q
 echo "Max Sequence Length:"
 MSL=49
 echo $MSL
+echo "Max Sequence Length:=${$MSL}"
 
 # The Held Out Pair index must be between 0 and 28 inclusive.
 HOP_IDX="$2"
 echo "HO"
-echo $HOP_IDX
+echo "HOP_IDX=${HOP_IDX}"
 
 EXP_NAME="no_text_no_vis"
 CKPT_DIR="snap/clevr/fromScratch/run${RUN}_clevr_ntnv_ho${HOP_IDX}_msl${MSL}_scale${MAX_Q}_steps481k_lr1e-5"
@@ -36,6 +37,7 @@ export PYTHONPATH=${PYTHONPATH}:src
 
 # Run eval on minimal splits
 python3 src/clevr_tasks/clevr.py --max_seq_length ${MSL} --dataset "clevr_atom_ho_exist" --optim none --test "val test" --load "${CKPT_DIR}/LAST" --experiment_name ${EXP_NAME} --ho_idx ${HO} --llayers 9 --xlayers 5 --rlayers 5 --batchSize 32 --tqdm --output ${CKPT_DIR} --clevr_config "src/clevr_tasks/symlink_config.yaml"
+# python3 src/clevr_tasks/clevr.py --max_seq_length ${MSL} --dataset "clevr_atom_ho_exist" --optim none --test "val test" --load "${CKPT_DIR}/LAST" --experiment_name ${EXP_NAME} --ho_idx ${HO} --llayers 9 --xlayers 5 --rlayers 5 --batchSize 32 --tqdm --output ${CKPT_DIR} --clevr_config "src/clevr_tasks/symlink_config.yaml"
 
 # Run eval on complex splits
-python3 src/clevr_tasks/clevr.py --max_seq_length ${MSL} --test "val test" --load "${CKPT_DIR}/LAST" --experiment_name ${EXP_NAME} --ho_idx ${HO} --llayers 9 --xlayers 5 --rlayers 5 --batchSize 32 --optim bert --tqdm --output ${CKPT_DIR} --clevr_config "src/clevr_tasks/symlink_config.yaml"
+# python3 src/clevr_tasks/clevr.py --max_seq_length ${MSL} --test "val test" --load "${CKPT_DIR}/LAST" --experiment_name ${EXP_NAME} --ho_idx ${HO} --llayers 9 --xlayers 5 --rlayers 5 --batchSize 32 --optim bert --tqdm --output ${CKPT_DIR} --clevr_config "src/clevr_tasks/symlink_config.yaml"
